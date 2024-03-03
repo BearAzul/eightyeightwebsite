e6218a895bf4c9b3bb64a324993db8812b969bf3
BearAzul <adyaabduazizulhakim@gmail.com>
2024-03-03 14:43:01 +0700

    eighty eight

---
+++ script.js
@@ -0,0 +1,31 @@
+// Sembunyikan dan Munculkan Item produk
+
+const show = document.getElementById("show");
+const item = document.getElementById("hide");
+const arrow = document.getElementById("arrow");
+const text = document.getElementById("text");
+
+show.addEventListener("click", () => {
+  if (item.style.display == "block") {
+    item.style.display = "none";
+    text.innerHTML = "Tampilkan Lebih Banyak...";
+    arrow.classList.remove("fa-caret-up");
+    arrow.classList.add("fa-sort-down");
+  } else {
+    item.style.display = "block";
+    text.innerHTML = "Tampilkan Lebih Sedikit...";
+    arrow.classList.remove("fa-sort-down");
+    arrow.classList.add("fa-caret-up");
+  }
+});
+
+// Button to Top
+const scrollBtn = document.querySelector(".to-home");
+
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop <= 120) {
+    scrollBtn.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+  }
+});
